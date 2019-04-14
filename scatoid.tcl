9452363f85d52d1ac67dab10e16321494fe6e3d2
Javi Garate <fjgarate@cimne.upc.edu>
2019-04-14 23:09:58 +0200

    wizard window

--- scatoid.tcl
+++ scatoid.tcl
@@ -2,16 +2,39 @@
 package require gid_smart_wizard
 
 namespace eval Scatoid {         
-    
+    variable dir
 }
 
 
-proc Scatoid::GeometryWindow { } { 
+proc Scatoid::Geometry { win } { 
+    smart_wizard::AutoStep $win Geometry
+}
+
+
+proc Scatoid::DrawGeometryButtonAction { } { 
     W "arroz"
 }
 
+proc Scatoid::InitWizard { } {   
+    variable dir
+
+    if { [GidUtils::IsTkDisabled] } {  
+        return
+    }          
+    smart_wizard::Init
+    smart_wizard::SetWizardNamespace "::Scatoid"
+    smart_wizard::SetWizardWindowName ".gid.scatoidwizard"
+    smart_wizard::SetWizardImageDirectory [file join $dir images]
+    smart_wizard::LoadWizardDoc [file join $dir scatoid.wiz]
+    smart_wizard::ImportWizardData
+
+    
+}
 
 proc Scatoid::AddToMenu { } {
+    variable dir
+    set dir [file dirname [info script]]
+
     if { [GidUtils::IsTkDisabled] } {
         return
     }   
@@ -26,8 +49,9 @@ proc Scatoid::AddToMenu { } {
     if { $position == -1 } {
         set position end
     }
-    GiDMenu::InsertOption Geometry [list Create Object Scatoid] $position PRE Scatoid::GeometryWindow "" "" insertafter _   
+    GiDMenu::InsertOption Geometry [list Create Object Scatoid] $position PRE smart_wizard::CreateWindow "" "" insertafter _   
 }
 
 Scatoid::AddToMenu
 GiDMenu::UpdateMenus
+Scatoid::InitWizard
